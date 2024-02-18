e3430ff30ce3fd8812800adaa9e5383c7cbc3736
Dadopoulos <71187766+MDadopoulos@users.noreply.github.com>
2024-02-18 23:28:10 +0200

    Update JPEG_encoder_decoder.py

--- JPEG_encoder_decoder.py
+++ JPEG_encoder_decoder.py
@@ -40,7 +40,7 @@ class JPEGtables:
 
 
 class JPEGencoded:
-    def __init__(self,blkType, indHor, indVer, huffStream):
+    def __init__(self,blkType,  indVer, indHor, huffStream):
         self.blkType = blkType
         self.indHor = indHor
         self.indVer = indVer
@@ -56,42 +56,50 @@ def channel_encode(img, qScale, JPEGenc, tables,blkType):
     tables (JPEGtables): The JPEGtables object.
     blkType (str): The type of the image channel.
     """
-
-    DCT_Y = np.zeros(img.shape)
-    quantized_Y = np.zeros(img.shape)
+    Dct_Y = np.zeros(img.shape).astype(int)
+    quantized_Y = np.zeros(img.shape).astype(int)
     DCpred = 0  # Example DC coefficient prediction
     if blkType == "Y":
         for i in range(0, len(img), 8):
             for j in range(0, len(img[0]), 8):
                 #apply DCT to each 8x8 block
-                DCT_Y[i:i+8, j:j+8] = blockDCT(img[i:i+8, j:j+8])
+                Dct_Y[i:i+8, j:j+8] = blockDCT(img[i:i+8, j:j+8])
                 #apply quantization to each 8x8 block
-                quantized_Y[i:i+8, j:j+8] = quantizeJPEG(DCT_Y[i:i+8, j:j+8], tables.qTableL, qScale)
+                quantized_Y[i:i+8, j:j+8] = quantizeJPEG(Dct_Y[i:i+8, j:j+8], tables.qTableL, qScale)
                 runSymbols = runLength(quantized_Y[i:i+8, j:j+8], DCpred)
-                DCpred = runSymbols[0][1]
+                #Set the DC coefficient prediction for the next block
+                DCpred = runSymbols[0][1] + DCpred
+                #encode the runSymbols using huffman encoding
                 huffStream=huffEnc(runSymbols, tables.DCL,tables.ACL)
+                #append the encoded block to the JPEGenc list
                 JPEGenc.append(JPEGencoded(blkType,i,j,huffStream))
     elif blkType == "Cr":
         for i in range(0, len(img), 8):
             for j in range(0, len(img[0]), 8):
                 #apply DCT to each 8x8 block
-                DCT_Y[i:i+8, j:j+8] = blockDCT(img[i:i+8, j:j+8])
+                Dct_Y[i:i+8, j:j+8] = blockDCT(img[i:i+8, j:j+8])
                 #apply quantization to each 8x8 block
-                quantized_Y[i:i+8, j:j+8] = quantizeJPEG(DCT_Y[i:i+8, j:j+8], tables.qTableC, qScale)
+                quantized_Y[i:i+8, j:j+8] = quantizeJPEG(Dct_Y[i:i+8, j:j+8], tables.qTableC, qScale)
                 runSymbols = runLength(quantized_Y[i:i+8, j:j+8], DCpred)
-                DCpred = runSymbols[0][1]
+                #Set the DC coefficient prediction for the next block
+                DCpred = runSymbols[0][1] + DCpred
+                #encode the runSymbols using huffman encoding
                 huffStream=huffEnc(runSymbols, tables.DCC,tables.ACC)
+                #append the encoded block to the JPEGenc list
                 JPEGenc.append(JPEGencoded(blkType,i,j,huffStream))
     elif blkType == "Cb":
         for i in range(0, len(img), 8):
             for j in range(0, len(img[0]), 8):
                 #apply DCT to each 8x8 block
-                DCT_Y[i:i+8, j:j+8] = blockDCT(img[i:i+8, j:j+8])
+                Dct_Y[i:i+8, j:j+8] = blockDCT(img[i:i+8, j:j+8])
                 #apply quantization to each 8x8 block
-                quantized_Y[i:i+8, j:j+8] = quantizeJPEG(DCT_Y[i:i+8, j:j+8], tables.qTableC, qScale)
+                quantized_Y[i:i+8, j:j+8] = quantizeJPEG(Dct_Y[i:i+8, j:j+8], tables.qTableC, qScale)
                 runSymbols = runLength(quantized_Y[i:i+8, j:j+8], DCpred)
-                DCpred = runSymbols[0][1]
+                #Set the DC coefficient prediction for the next block
+                DCpred = runSymbols[0][1] + DCpred
+                #encode the runSymbols using huffman encoding
                 huffStream=huffEnc(runSymbols, tables.DCC,tables.ACC)
+                #append the encoded block to the JPEGenc list
                 JPEGenc.append(JPEGencoded(blkType,i,j,huffStream))
     else:
         print("Invalid block type")
@@ -104,7 +112,7 @@ def JPEGencode(img,  qScale, subImg):
     Parameters:
     img (numpy.ndarray): The input image.
     qScale (float): The quantization scale.
-    subImg (int): The subsampling factor.
+    subImg (list): 1x3 matrix for subsampling [4, 2, 0], [4, 2, 2], [4, 4, 4].
     Returns:
     JPEGenc: A touple with N+1 elements, where N is the number of blocks in the image.
     """
@@ -132,11 +140,35 @@ def JPEGencode(img,  qScale, subImg):
 
     return tuple(JPEGenc)
 
-def JPEGdecode(JPEGenc):
+def retrieve_subImg(JPEGenc):
+    """
+    Retrieves the subsampling factor from the JPEGenc object.
+    Parameters:
+    JPEGenc: A touple with N+1 elements, where N is the number of blocks in the image.
+    Returns:
+    subImg (list): 1x3 matrix for subsampling [4, 2, 0], [4, 2, 2], [4, 4, 4].
+    """
+
+    blocksY = len(list(filter(lambda obj:  obj.blkType == "Y" , JPEGenc[1:])))
+    blocksCr = len(list(filter(lambda obj:  obj.blkType == "Cr" , JPEGenc[1:])))
+    blocksCb = len(list(filter(lambda obj:  obj.blkType == "Cb" , JPEGenc[1:])))
+    if blocksY == blocksCr and blocksY == blocksCb:
+        subImg = [4, 4, 4]
+    elif blocksY == 2 * blocksCr and blocksY == 2 * blocksCb:
+        subImg = [4, 2, 2]
+    elif blocksY == 4 * blocksCr and blocksY == 4 * blocksCb:
+        subImg = [4, 2, 0]
+    else:
+        raise ValueError("Unsupported subsampling format")
+    return subImg
+
+
+def JPEGdecode(JPEGenc,qScale):
     """
     Decodes an image using the JPEG standard.
     Parameters:
     JPEGenc: A touple with N+1 elements, where N is the number of blocks in the image.
+    qScale (float): The quantization scale.
     Returns:
     numpy.ndarray: The reconstructed image.
     """
@@ -151,8 +183,32 @@ def JPEGdecode(JPEGenc):
     ACL = JPEGenc[0].ACL
     ACC = JPEGenc[0].ACC
     
-    # Initialize list for reconstructed image
-    imgRec = []
+    # Retrieve the subsampling factor
+    subImg = retrieve_subImg(JPEGenc)
+
+    # Retrieve the dimensions of the image
+    width = max(JPEGenc[1:], key=lambda obj: obj.indHor).indHor + 8 
+    height = max(JPEGenc[1:], key=lambda obj: obj.indVer).indVer + 8
+
+    # Initialize list for YCrCb components
+    imageY = np.zeros((height, width))
+
+    if subImg == [4, 4, 4]:
+        imageCr = np.zeros((height, width))
+        imageCb = np.zeros((height, width))
+    elif subImg == [4, 2, 2]:
+        imageCr = np.zeros((height, width//2))
+        imageCb = np.zeros((height, width//2))
+    elif subImg == [4, 2, 0]:
+        imageCr = np.zeros((height//2, width//2))
+        imageCb = np.zeros((height//2, width//2))
+    else:
+        raise ValueError("Unsupported subsampling format")
+    
+    # Initialize DC prediction values
+    DCpredY = 0
+    DCpredCr = 0
+    DCpredCb = 0
     
     # Decode each channel
     for i in range(1, len(JPEGenc)):
@@ -162,27 +218,69 @@ def JPEGdecode(JPEGenc):
         indVer = JPEGenc[i].indVer
         huffStream = JPEGenc[i].huffStream
         
-        # Decode Huffman stream
         if blkType == "Y":
+            # Decode Huffman stream
             runSymbols = huffDec(huffStream, DCL, ACL)
-            DCpred = 0
-            quantized_Y = irunLength(runSymbols, DCpred)
-            iDCT_Y = dequantize_channel(quantized_Y, qTableL, 1)
-            imgRec.append(iBlockDCT(iDCT_Y))
+            # Decode run-length symbols
+            quantized_Y = irunLength(runSymbols, DCpredY)
+            # Update DC prediction
+            DCpredY = runSymbols[0][1] + DCpredY #or quantized_Y[0, 0]
+            # Dequantize the coefficients
+            dequantized_Y = dequantizeJPEG(quantized_Y, qTableL, qScale)
+            #apply inverse DCT to the 8x8 block
+            imageY[indVer:indVer+8, indHor:indHor+8] = iBlockDCT(dequantized_Y)
+
         elif blkType == "Cr":
+            # Decode Huffman stream
             runSymbols = huffDec(huffStream, DCC, ACC)
-            DCpred = 0
-            quantized_Cr = irunLength(runSymbols, DCpred)
-            iDCT_Cr = dequantize_channel(quantized_Cr, qTableC, 1)
-            imgRec.append(iBlockDCT(iDCT_Cr))
+            # Decode run-length symbols
+            quantized_Cr = irunLength(runSymbols, DCpredCr)
+            # Update DC prediction
+            DCpredCr = runSymbols[0][1] + DCpredCr #or quantized_Cr[0, 0]
+            # Dequantize the coefficients
+            dequantized_Cr = dequantizeJPEG(quantized_Cr, qTableC, qScale)
+            #apply inverse DCT to the 8x8 block
+            imageCr[indVer:indVer+8, indHor:indHor+8] = iBlockDCT(dequantized_Cr)
+
         elif blkType == "Cb":
+            # Decode Huffman stream
             runSymbols = huffDec(huffStream, DCC, ACC)
-            DCpred = 0
-            quantized_Cb = irunLength(runSymbols, DCpred)
-            iDCT_Cb = dequantize_channel(quantized_Cb, qTableC, 1)
-            imgRec.append(iBlockDCT(iDCT_Cb))
+            # Decode run-length symbols
+            quantized_Cb = irunLength(runSymbols, DCpredCb)
+            # Update DC prediction
+            DCpredCb = runSymbols[0][1] + DCpredCb #or quantized_Cb[0, 0]
+            # Dequantize the coefficients
+            dequantized_Cb = dequantizeJPEG(quantized_Cb, qTableC, qScale)
+            #apply inverse DCT to the 8x8 block
+            imageCb[indVer:indVer+8, indHor:indHor+8] = iBlockDCT(dequantized_Cb)
+
         else:
             print("Invalid block type")
     
+    # Convert YCrCb components back to an RGB image
+    imgRec = convert2rgb(imageY, imageCr, imageCb, subImg)
+
     # Return the reconstructed image
     return imgRec
+
+
+if __name__ == "__main__" :
+
+    # Load the image
+    img = cv2.imread('baboon.png')
+    image_RGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    
+    plt.imshow(image_RGB)
+    plt.show()
+
+    # Define the quantization scale
+    qScale = 1
+
+    # Define the subsampling factor
+    subImg = [4, 4, 4]
+
+    # Encode the image
+    JPEGenc=JPEGencode(image_RGB,  qScale, subImg)
+    imgRec=JPEGdecode(JPEGenc,qScale)
+    plt.imshow(imgRec)
+    plt.show()
